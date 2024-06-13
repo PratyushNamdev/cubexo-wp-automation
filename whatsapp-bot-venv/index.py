a6b62b7e773c27e9730ca3ab4ae3e5821ae67b2e
Pratyush Namdev <pratyushnamdev140@gmail.com>
2024-06-13 12:39:43 +0530

    Inital commit

---
+++ whatsapp-bot-venv/index.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+from flask_lt import run_with_lt  # Import run_with_lt from flask_localtunnel
+
+app = Flask(__name__)
+run_with_lt(app)  # Integrate localtunnel with your Flask app
+
+@app.route('/bot', methods=['POST'])
+def bot():
+    incoming_msg = request.values.get('Body', '').lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+    if 'quote' in incoming_msg:
+        # return a quote
+        r = requests.get('https://api.quotable.io/random')
+        if r.status_code == 200:
+            data = r.json()
+            quote = f'{data["content"]} ({data["author"]})'
+        else:
+            quote = 'I could not retrieve a quote at this time, sorry.'
+        msg.body(quote)
+        responded = True
+    if 'cat' in incoming_msg:
+        # return a cat pic
+        msg.media('https://cataas.com/cat')
+        responded = True
+    if not responded:
+        msg.body('I only know about famous quotes and cats, sorry!')
+    return str(resp)
+
+if __name__ == '__main__':
+    app.run()
